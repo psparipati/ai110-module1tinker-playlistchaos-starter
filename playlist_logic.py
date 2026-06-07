685863032a0f09aaafeceba229297036fc4670e0
Prabhav Sai Paripati <psparipati@gmail.com>
2026-06-07 17:06:18 -0500

    modified:   __pycache__/playlist_logic.cpython-314.pyc 	modified:   playlist_logic.py

--- playlist_logic.py
+++ playlist_logic.py
@@ -168,7 +168,7 @@ def search_songs(
 
     for song in songs:
         value = str(song.get(field, "")).lower()
-        if value and value in q:
+        if value and value in value:
             filtered.append(song)
 
     return filtered
